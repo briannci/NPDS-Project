7bb761936f5e21ec4b3eaf3b3853b3515e432e10
briannci <x12533897@student.ncirl.ie>
2015-11-24 14:13:03 +0000

    First commit

---
+++ register.php
@@ -0,0 +1,44 @@
+<?
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Dropbox Clone</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+   
+
+  </head>
+  <body>
+
+	  <form class="form col-md-6 col-md-offset-3">
+	  	<h1>File Storage Clone</h1><br/>
+        <h2>Register an account</h2>
+            <div class="form-group">
+              <input type="text" class="form-control input-lg" placeholder="Username:">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control input-lg" placeholder="Password:">
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary btn-lg btn-block">Register</button>
+              <span class="pull-right"><a href="index.php">Login</a></span>
+            </div>
+        </form>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap"></script>
+
+  </body>
+
+  </html>
