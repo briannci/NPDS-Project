501469135b5b460bd6a7804e237571b9c2bff45e
Sean Keeley <sean.keeley1@gmail.com>
2015-12-07 13:10:34 +0000

    Testing Git within PHP Strom

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
           <title>File Storage</title>
+          <!-- Testing Git in PHPStorm-->
           <!-- Bootstrap -->
           <link href="css/bootstrap.min.css" rel="stylesheet">
             <link href="css/custom.css" rel="stylesheet">
